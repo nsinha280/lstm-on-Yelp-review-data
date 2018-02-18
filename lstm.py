958b49954d601ab1dd9a97867d1c9454504c1c52
NIMESH SINHA <nimesh@NIMESHs-MacBook-Pro.local>
2018-02-18 13:10:48 -0800

    update

--- lstm.py
+++ lstm.py
@@ -24,54 +24,50 @@ def convert(x):
                 ob['%s_%s' % (k, kk)] = vv
             del ob[k]
     return ob
-json_filename='/Users/nimesh/Downloads/dataset/review.json'
+json_filename='review.json'
 with open(json_filename,'rb') as f:
     data = f.readlines()
+
+#Converting into pandas dataframe and filtering only text and ratings given by the users
 df = pd.DataFrame([convert(line) for line in data])
-
-
 data = df[['text', 'stars']]
 
-#If rating is above 3, its considered as positive.
+#I have considered a rating above 3 as positive and less than or equal to 3 as negative.
 
 data['sentiment']=['pos' if (x>3) else 'neg' for x in data['stars']]
 data['text'] = data['text'].apply(lambda x: x.lower())
 data['text'] = data['text'].apply((lambda x: re.sub('[^a-zA-z0-9\s]','',x)))
-
 for idx,row in data.iterrows():
     row[0] = row[0].replace('rt',' ')
-
 data['text']= [x.encode('ascii') for x in data['text']]
 
-
-max_fatures = 2000
-tokenizer = Tokenizer(nb_words=max_fatures, split=' ')
+tokenizer = Tokenizer(nb_words = 2500, split=' ')
 tokenizer.fit_on_texts(data['text'].values)
+#print(tokenizer.word_index)  # To see the dicstionary
 X = tokenizer.texts_to_sequences(data['text'].values)
 X = pad_sequences(X)
 
 embed_dim = 128
-lstm_out = 196
+lstm_out = 300
+batch_size= 32
 
 ##Buidling the LSTM network
 
 model = Sequential()
-model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1], dropout=0.2))
-model.add(LSTM(lstm_out, dropout_U=0.2, dropout_W=0.2))
+model.add(Embedding(2500, embed_dim,input_length = X.shape[1], dropout=0.1))
+model.add(LSTM(lstm_out, dropout_U=0.1, dropout_W=0.1))
 model.add(Dense(2,activation='softmax'))
 model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
 
 Y = pd.get_dummies(data['sentiment']).values
-X_train, X_validt, Y_train, Y_valid = train_test_split(X,Y, test_size = 0.33, random_state = 42)
-print(X_train.shape,Y_train.shape)
-print(X_test.shape,Y_test.shape)
+X_train, X_valid, Y_train, Y_valid = train_test_split(X,Y, test_size = 0.20, random_state = 36)
 
 #Here we train the Network.
 
-model.fit(X_train, Y_train, nb_epoch = 7, batch_size=batch_size, verbose = 2)
+model.fit(X_train, Y_train, batch_size =batch_size, nb_epoch = 1,  verbose = 5)
 
 # Measuring score and accuracy on validation set
 
-score,acc = model.evaluate(X_valid, Y_test, verbose = 2, batch_size = batch_size)
-print("score: %.2f" % (score))
-print("acc: %.2f" % (acc))
+score,acc = model.evaluate(X_valid, Y_valid, verbose = 2, batch_size = batch_size)
+print("Logloss score: %.2f" % (score))
+print("Validation set Accuracy: %.2f" % (acc))
